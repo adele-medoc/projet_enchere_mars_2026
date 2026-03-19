f1b24256c31d3111b61d0c4287599451d8a8c123
Adele MEDOC <adelemedoc@gmail.fr>
2026-03-19 09:10:27 +0100

    ajout template detailVente

--- src/main/java/fr/eni/projetenchere/dal/DaoEnchereJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/DaoEnchereJdbcImpl.java
@@ -1,4 +1,7 @@
 package fr.eni.projetenchere.dal;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DaoEnchereJdbcImpl {
 }
--- src/main/java/fr/eni/projetenchere/service/VenteServiceJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/service/VenteServiceJdbcImpl.java
@@ -1,11 +1,44 @@
 package fr.eni.projetenchere.service;
 
+import fr.eni.projetenchere.bo.Article;
+import fr.eni.projetenchere.bo.Categorie;
+import fr.eni.projetenchere.bo.Utilisateur;
+import fr.eni.projetenchere.dal.DaoArticle;
+import fr.eni.projetenchere.dal.DaoUtilisateur;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VenteServiceJdbcImpl implements VenteService {
-    @Override
-    public void CreerNouvelleVente() {
 
+    @Autowired
+    DaoArticle daoArticle;
+    @Autowired
+    DaoUtilisateur daoUtilisateur;
+
+    @Override
+    public void CreerNouvelleVente(Article article) {
+    }
+
+    @Override
+    public Article supprimerVente() {
+        return null;
+    }
+
+    @Override
+    public List<Categorie> consulterCategories() {
+        return List.of();
+    }
+
+    @Override
+    public List<Article> consulterArticles() {
+        return List.of();
+    }
+
+    @Override
+    public Utilisateur consulterUserAuthentifier() {
+        return null;
     }
 }
