6e02ac14dd27039b8379c17fab09a7deab7b4c67
Adele MEDOC <adelemedoc@gmail.fr>
2026-03-23 16:52:33 +0100

    fix authetification issues, creation of object ok and authentification with the db ok

--- src/main/java/fr/eni/projetenchere/dal/DaoAdresseJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/DaoAdresseJdbcImpl.java
@@ -1,7 +1,46 @@
 package fr.eni.projetenchere.dal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DaoAdresseJdbcImpl {
+public class DaoAdresseJdbcImpl implements DaoAdresse {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static String INSERT_ADRESSE = "  INSERT INTO ADRESSE(rue_adresse,code_postale_adresse,ville_adresse) VALUES (:rue,:codePostal,:ville)";
+    private static String SELECT_ADRESSE_BY_ID = " SELECT * FROM ADRESSE WHERE id_adresse =  ";
+
+    public void insertAdresse(String rue,String codePostal, String ville){
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("rue",rue)
+                .addValue("codePostal",codePostal)
+                .addValue("ville",ville);
+        namedParameterJdbcTemplate.update(INSERT_ADRESSE,params);
+    }
+
+    public long insertAdresseAndGetID(String rue,String codePostal, String ville){
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("rue",rue)
+                .addValue("codePostal",codePostal)
+                .addValue("ville",ville);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(INSERT_ADRESSE,params,keyHolder);
+
+        if(keyHolder !=null && keyHolder.getKey()!=null){
+            long idAdresse= keyHolder.getKey().longValue();
+            return idAdresse;
+        }else{
+            return 0;
+        }
+    }
 }
--- src/main/java/fr/eni/projetenchere/security/SecurityConfiguration.java
+++ src/main/java/fr/eni/projetenchere/security/SecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -19,32 +21,12 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-
-    @Bean // on définit un bean pour l'utilitaire d'encryption de mot de passe
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean // on définit un bean pour la gestion des utilisateurs en mémoire
-    public InMemoryUserDetailsManager userDetailsService() {
-        List<UserDetails> userDetailsList = new ArrayList<>();
-        /**
-         * Création de 2 utilistaurs en mémoire de type UserDetials
-         * Pour chaque utilistauer on définit: un username, un password encodé avec passwordEncoder, liste de rôles
-         */
-        userDetailsList.add(User.withUsername("membre").password(passwordEncoder().encode("membre"))
-                .roles("user").build());
-        userDetailsList.add(User.withUsername("admin").password(passwordEncoder().encode("admin"))
-                .roles("admin", "user").build());
-        return new InMemoryUserDetailsManager(userDetailsList);
-    }
-
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // http.authorizeHttpRequests() : défini quelle requête HTTP j'autorise
         http.authorizeHttpRequests((authorize) -> authorize
-
+                        .requestMatchers("/categories/gestion","/utilisateur/gestion").hasRole("admin")
+                        .requestMatchers("/enchere/nouvelArticle").authenticated()
                         .requestMatchers("/**").permitAll())
                 // on effectue une authentification basique (user/mdp)
                 .httpBasic(Customizer.withDefaults())
@@ -57,4 +39,25 @@ public class SecurityConfiguration {
                 .logout((logout) -> logout.logoutSuccessUrl("/"));
         return http.build();
     }
+    @Bean // on définit un bean pour l'utilitaire d'encryption de mot de passe
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //return new BCryptPasswordEncoder();
+    }
+
 }
+    //    @Bean // on définit un bean pour la gestion des utilisateurs en mémoire
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        List<UserDetails> userDetailsList = new ArrayList<>();
+//        /**
+//         * Création de 2 utilistaurs en mémoire de type UserDetials
+//         * Pour chaque utilistauer on définit: un username, un password encodé avec passwordEncoder, liste de rôles
+//         */
+//        userDetailsList.add(User.withUsername("membre").password(passwordEncoder().encode("membre"))
+//                .roles("user").build());
+//        userDetailsList.add(User.withUsername("admin").password(passwordEncoder().encode("admin"))
+//                .roles("admin", "user").build());
+//        return new InMemoryUserDetailsManager(userDetailsList);
+//    }
+
+//}
