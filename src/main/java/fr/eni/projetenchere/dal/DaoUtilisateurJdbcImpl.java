0907c8c1a84f30cf966cd5b7fa1ed9a4470a5b36
chaer4real <shaq.63@hotmail.fr>
2026-03-24 15:36:50 +0100

    Modification - 24/03/2026 partie 2

--- src/main/java/fr/eni/projetenchere/dal/DaoUtilisateurJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/DaoUtilisateurJdbcImpl.java
@@ -21,10 +21,10 @@ public class DaoUtilisateurJdbcImpl implements DaoUtilisateur{
             WHERE id_utilisateur = ?
             """;
     private static final String SELECT_BY_USERNAME = """
-            SELECT id_utilisateur, pseudo_utilisateur,nom_utilisateur,prenom_utilisateur,email_utilisateur,telephone_utilisateur ,mot_de_passe_utilisateur,credit_utilisateur,administrateur_utilisateur, ADRESSE.id_adresse ,rue_adresse,code_postale_adresse,ville_adresse
+            SELECT id_utilisateur, username_utilisateur,nom_utilisateur,prenom_utilisateur,email_utilisateur,telephone_utilisateur ,mot_de_passe_utilisateur,credit_utilisateur,administrateur_utilisateur, ADRESSE.id_adresse ,rue_adresse,code_postale_adresse,ville_adresse
             FROM UTILISATEUR
             Join ADRESSE on ADRESSE.id_adresse = UTILISATEUR.id_adresse
-            WHERE pseudo_utilisateur =?;
+            WHERE username_utilisateur =?;
             """;
 
     private static final String INSERT = "insert into UTILISATEUR (username_utilisateur, nom_utilisateur, prenom_utilisateur, email_utilisateur, telephone_utilisateur, mot_de_passe_utilisateur, credit_utilisateur, administrateur_utilisateur) values (?, ?, ?, ?, ?, ?, ?, ?)";
